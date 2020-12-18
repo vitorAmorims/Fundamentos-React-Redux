c0f3fb4ef3b47412c1c153d45e71a53b3b8d502a
vitorAmorims <vtamorims@gmail.com>
2020-12-18 16:27:17 -0300

    conectando componentes com Redux #02

--- redux-simples/src/components/Intervalo.jsx
+++ redux-simples/src/components/Intervalo.jsx
@@ -1,21 +1,25 @@
 import "./Intervalo.css";
 import React from "react";
+import { connect } from "react-redux";
 
-export default (props) => {
+const Intervalo = (props) => {
+  // console.log(props);
+  const { min, max } = props;
   return (
     <div className="intervalo">
       <span>Minimo:</span>
-      <input
-        type="number"
-        value={10}
-        readOnly
-      />
+      <input type="number" value={ min } readOnly />
       <span>Máximo:</span>
-      <input
-        type="number"
-        value={20}
-        readOnly
-      />
+      <input type="number" value={ max } readOnly />
     </div>
   );
 };
+
+function mapStateToProps(state) {
+  return {
+    min: state.numeros.min,
+    max: state.numeros.max,
+  }
+}
+
+export default connect(mapStateToProps)(Intervalo);
